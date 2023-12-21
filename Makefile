3b5aede9283a1e5485fa0a423ff335ec5b3b25c6
Glenn Hickey <glenn.hickey@gmail.com>
2023-12-21 12:53:25 -0500

    basic loading

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ rootPath = ./
 
 all : vg2maf
 
+libbdsgPath = deps/libbdsg-easy
+libbdsgLibs = ${libbdsgPath}/lib/libbdsg.a ${libbdsgPath}/lib/libhandlegraph.a ${libbdsgPath}/lib/libsdsl.a ${libbdsgPath}/lib/libdivsufsort.a ${libbdsgPath}/lib/libdivsufsort64.a
+
 CXX ?= g++
 CXXFLAGS += -std=c++14 -I deps/taffy/taffy/inc -I deps/libbdsg-easy/include -I deps/libvgio/include/ 
 
@@ -34,14 +37,14 @@ vg2maf.o : vg2maf.cpp
 deps/taffy/lib/libstTaf.a:
 	cd deps/taffy && ${MAKE}
 
-deps/libbdsg-easy/lib/libbdsg.a:
+${libbdsgPath}/lib/libbdsg.a:
 	cd deps/libbdsg-easy && ${MAKE}
 
 deps/libvgio/build/libvgio.a:
 	cd deps/libvgio && rm -rf build && mkdir build && cd build && cmake .. && ${MAKE}
 
-vg2maf : vg2maf.o deps/taffy/lib/libstTaf.a deps/libbdsg-easy/lib/libbdsg.a deps/libvgio/build/libvgio.a
-	${CXX} ${CXXFLAGS} -fopenmp -pthread vg2maf.o deps/taffy/lib/libstTaf.a deps/libbdsg-easy/lib/libbdsg.a deps/libvgio/build/libvgio.a -o vg2maf
+vg2maf : vg2maf.o deps/taffy/lib/libstTaf.a ${libbdsgPath}/lib/libbdsg.a deps/libvgio/build/libvgio.a
+	${CXX} ${CXXFLAGS} -lm -lz -llzma -lbz2 -ldeflate -fopenmp -pthread vg2maf.o deps/taffy/lib/libstTaf.a ${libbdsgLibs} -ljansson deps/libvgio/build/libvgio.a -lprotobuf -lhts -o vg2maf
 
 #test :
 #	make
--- vg2maf.cpp
+++ vg2maf.cpp
@@ -1,6 +1,128 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <fstream>
+#include <memory>
+#include <unistd.h>
+#include <getopt.h>
+
+
 using namespace std;
 
+#include "handlegraph/path_handle_graph.hpp"
+#include "bdsg/packed_graph.hpp"
+#include "bdsg/hash_graph.hpp"
+#include "bdsg/snarl_distance_index.hpp"
+#include "vg/io/vpkg.hpp"
+
+using namespace std;
+using namespace handlegraph;
+using namespace bdsg;
+
+// from hal2vg/clip-vg.cpp
+static unique_ptr<PathHandleGraph> load_graph(istream& graph_stream);
+
+void help(char** argv) {
+  cerr << "usage: " << argv[0] << " [options] <graph> <distance-index>" << endl
+       << "Chop out path intervals from a vg graph" << endl
+       << endl
+       << "options: " << endl
+       << "    -p, --progress            Print progress" << endl
+       << endl;
+}    
+
+
 int main(int argc, char** argv) {
-    cerr << "hello" << endl;
+
+    bool progress = false;
+    int c;
+    optind = 1; 
+    while (true) {
+
+        static const struct option long_options[] = {
+            {"help", no_argument, 0, 'h'},
+            {"progress", no_argument, 0, 'p'},
+            {0, 0, 0, 0}
+        };
+
+        int option_index = 0;
+
+        c = getopt_long (argc, argv, "hp",
+                         long_options, &option_index);
+
+        // Detect the end of the options.
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+        case 'p':
+            progress = true;
+            break;
+        case 'h':
+        case '?':
+            /* getopt_long already printed an error message. */
+            help(argv);
+            exit(1);
+            break;
+        default:
+            abort ();
+        }
+    }
+
+    if (argc <= 2) {
+        help(argv);
+        return 1;
+    }
+
+    string graph_path = argv[optind++];
+    ifstream graph_stream(graph_path);
+    if (!graph_stream) {
+        cerr << "[vg2maf] error: Unable to open input graph " << graph_path << endl;
+        return 1;
+    }    
+    unique_ptr<PathHandleGraph> graph = load_graph(graph_stream);
+    graph_stream.close();
+    if (progress) {
+        cerr << "[vg2maf]: Loaded graph" << endl;
+    }
+
+    string distance_index_filename = argv[optind++];
+    SnarlDistanceIndex distance_index;
+    distance_index.deserialize(distance_index_filename);
+    if (progress) {
+        cerr << "[vg2maf]: Loaded distance index" << endl;
+    }
+
+    
+    
+}
+
+
+
+
+
+
+
+unique_ptr<PathHandleGraph> load_graph(istream& graph_stream) {
+
+    char magic_bytes[4];
+    graph_stream.read(magic_bytes, 4);
+    uint32_t magic_number = ntohl(*((uint32_t*) magic_bytes));
+    graph_stream.clear();
+    graph_stream.seekg(0, ios::beg);
+
+    PathHandleGraph* graph;
+    if (magic_number == PackedGraph().get_magic_number()) {
+        graph = new PackedGraph();
+    } else if (magic_number == HashGraph().get_magic_number()) {
+        graph = new HashGraph();
+    }  else {
+        cerr << "Unable to parse input graph with magic number " << magic_number << endl;
+        exit(1);
+    }
+    dynamic_cast<SerializableHandleGraph*>(graph)->deserialize(graph_stream);
+
+    return unique_ptr<PathHandleGraph>(graph);
 }
