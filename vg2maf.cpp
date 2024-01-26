7fa3bd12f9ada0c265d848d14c16890da7d80b64
Glenn Hickey <glenn.hickey@gmail.com>
2024-01-26 15:38:53 -0500

    ratchet down buffer sizes

--- vg2maf.cpp
+++ vg2maf.cpp
@@ -23,9 +23,9 @@ extern "C" {
 }
 
 // number of nodes to scan before sending to parallel batch
-static const int64_t node_buffer_size = 250000;
+static const int64_t node_buffer_size = 25000;
 // number of bases for each gam index query
-static const int64_t gam_idx_query_bp = 10000;
+static const int64_t gam_idx_query_bp = 5000;
 // biggest hole allowable in id range passed to gam index
 static const int64_t gam_idx_max_gap = 2;
 // maximum read depth (to prevent memory issues)
